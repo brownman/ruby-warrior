be07b7f832a6d065be57831481cd988afe9d5c19
kiba <wikipediankiba@gmail.com>
2009-01-23 05:59:39 -0800

    load the file by default

--- graphics/engine.rb
+++ graphics/engine.rb
@@ -3,6 +3,7 @@
 class Engine
   def initialize
     @screen = Screen.new([800,600],0,[Rubygame::HWSURFACE,Rubygame::DOUBLEBUF])
+    @resource = Graphic_Resource.new()
     @map = Map.new()
   end
 end
--- graphics/graphic_resource.rb
+++ graphics/graphic_resource.rb
@@ -3,11 +3,9 @@
 class Graphic_Resource
   attr_accessor :config_file
   def initialize
-    @config_name = "data.yml"
+    @config_name = "data/data.yml"
     @config_file = nil
-  end
-  def read filename
-    yam_string = File.read(filename)
+    yam_string = File.read(@config_name)
     @config_file = YAML::load(yaml_string)
   end
 end
