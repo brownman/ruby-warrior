44c4846561d5ea9c3554bd7f65c5177592757689
kiba <wikipediankiba@gmail.com>
2009-01-24 23:30:38 -0800

    add a method for calculating position from a value

--- graphics/position_calculate.rb
+++ graphics/position_calculate.rb
@@ -1,9 +1,21 @@
 
 
 def Position_Calculate
-  def position x , y
+  def translate_into_position x , y
     x = x * 50
     y = y * 50
+    return x , y
+  end
+  def get_position_from_value width , height , v
+    x = 0
+    y = 0
+    v.times do
+      x += 50
+      if x == width
+	x = 0
+        y += 50
+      end
+    end
     return x , y
   end
 end
