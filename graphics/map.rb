6e17ae2b5a5efe8ba1cadf3c72698a95424b51e7
kiba <wikipediankiba@gmail.com>
2009-01-24 23:20:50 -0800

    add tiles based on info from @Map

--- graphics/map.rb
+++ graphics/map.rb
@@ -13,4 +13,12 @@ class Map
   def create
     @map = Array.new(@floor.width*@floor.height,0)
   end
+  def set_map
+    @map.each do |map|
+      if map == 0
+	object = MapTiles.new(@resource.name('floor.png'))
+	@object << object
+      end
+    end
+  end
 end
