1ad60a0b0ee272031e0ccbf4f31a178aa6efd503
kiba <wikipediankiba@gmail.com>
2009-01-27 05:59:42 -0800

    add set position for player

--- graphics/units.rb
+++ graphics/units.rb
@@ -8,5 +8,8 @@ class Units
   end
   def set_player warrior
     @player = Tile.new(@resource.name("player"))
+    x , y = @position.translate_into_position(warrior.position[0],warrior.position[1])
+    @player.rect.x = x
+    @player.rect.y = y
   end
 end
