5f4dd38f600f698f9a2a265e274c31926774cfd0
kiba <wikipediankiba@gmail.com>
2009-01-27 05:51:37 -0800

    add a method for setting up sprite version of player and add Position Calculate class

--- graphics/units.rb
+++ graphics/units.rb
@@ -4,5 +4,9 @@ class Units
   def initialize resource , screen
     @resource = resource
     @screen = screen
+    @position = Position_Calculate.new()
+  end
+  def set_player warrior
+    @player = Tile.new(@resource.name("player"))
   end
 end
