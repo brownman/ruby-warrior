d7eaa05927aa9c6e68f685f90e9c45eb4ea7d685
kiba <wikipediankiba@gmail.com>
2009-01-29 05:57:32 -0800

    add the guts

--- graphics/unitsprite.rb
+++ graphics/unitsprite.rb
@@ -1,5 +1,9 @@
 
 class UnitSprite
-  def initialize
+  def initialize image , unit
+    super()
+    @unit = unit
+    @image = Rubygame::Surface.load(image)
+    @rect = Rect.new(0,0,*image.size)
   end
 end
