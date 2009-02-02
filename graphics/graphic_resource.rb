0779f22fa4d7bd5e294ca44dc8500e3d4e7b7e70
kiba <wikipediankiba@gmail.com>
2009-02-02 06:02:45 -0800

    add another yaml file to load

--- graphics/graphic_resource.rb
+++ graphics/graphic_resource.rb
@@ -1,12 +1,16 @@
 
 
 class Graphic_Resource
-  attr_accessor :data_file
+  attr_accessor :data_file , :config_file
   def initialize
     @data_file = nil
+    @config_file = nil
     data_name = "data/data.yml"
+    config_name = "data/config.yml"
     yaml_string = File.read(data_name)
     @data_file = YAML::load(yaml_string)
+    yaml_string = File.read(config_name)
+    @config_file = YAML::load(yaml_string)
   end
   def name name
     return @data_file[name]
