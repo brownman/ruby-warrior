7c5d7b33746086dd2ca889d7e62e529a956bc454
kiba <wikipediankiba@gmail.com>
2009-02-02 18:23:00 -0800

    remove old cruft

--- graphics/engine.rb
+++ graphics/engine.rb
@@ -4,19 +4,13 @@ class Engine
   attr_accessor :maps , :units , :runnable
   def initialize
     @resource = Graphic_Resource.new()
-    @runnable = false
-    if @resource.start?
-      @screen = Screen.new([800,600],0,[Rubygame::HWSURFACE,Rubygame::DOUBLEBUF])
-      @maps = Maps.new(@resource,@screen)
-      @units = Units.new(@resource,@screen)
-      @runnable = true
-    end
+    @screen = Screen.new([800,600],0,[Rubygame::HWSURFACE,Rubygame::DOUBLEBUF])
+    @maps = Maps.new(@resource,@screen)
+    @units = Units.new(@resource,@screen)
   end
   def draw
-    if @runnable == true
-      @maps.draw()
-      @units.draw()
-      @screen.flip()
-    end
+    @maps.draw()
+    @units.draw()
+    @screen.flip()
   end
 end
