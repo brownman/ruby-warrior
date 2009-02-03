c90c9979feced13a396cd8f8c3aabe9f4d388833
kiba <wikipediankiba@gmail.com>
2009-02-02 17:30:09 -0800

    add much

--- graphics/config.rb
+++ graphics/config.rb
@@ -7,6 +7,22 @@ class Graphic_Resource
     @config = YAML::Load(yaml_string)
   end
   def start?
-    return @config_file["graphic"]
+    @config_file["graphic"] if
+    graphic_set()
   end
+  def dependency_set
+    require"rubygame"
+    include Rubygame
+  end
+  def graphic_set
+    require 'graphics/graphics_resource.rb'
+    require 'graphics/graphics_engine.rb'
+    require 'graphics/engine.rb'
+    require 'graphics/tile.rb'
+    require 'graphics/position_calculate.rb'
+    require 'graphics/maps.rb'
+    require 'graphics/unitsprite.rb'
+    require 'graphics/units.rb'
+  end
+  
 end
