a6c842d16d09ead95d78620459a28ebec1a2cd93
kiba <wikipediankiba@gmail.com>
2009-02-02 17:02:17 -0800

    use true instead of 1

--- graphics/engine.rb
+++ graphics/engine.rb
@@ -1,12 +1,16 @@
 
 
 class Engine
-  attr_accessor :maps , :units
+  attr_accessor :maps , :units , :runnable
   def initialize
     @resource = Graphic_Resource.new()
-    @screen = Screen.new([800,600],0,[Rubygame::HWSURFACE,Rubygame::DOUBLEBUF])
-    @maps = Maps.new(@resource,@screen)
-    @units = Units.new(@resource,@screen)
+    @runnable = false
+    if @resource.start?
+      @screen = Screen.new([800,600],0,[Rubygame::HWSURFACE,Rubygame::DOUBLEBUF])
+      @maps = Maps.new(@resource,@screen)
+      @units = Units.new(@resource,@screen)
+      @runnable = true
+    end
   end
   def draw
     @maps.draw()
